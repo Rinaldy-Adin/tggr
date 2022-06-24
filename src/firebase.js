df2633919b5b49f620a1ae53ae88b34435dd9939
Rinaldy Adin <16521390@mahasiswa.itb.ac.id>
2022-06-24 07:14:29 +0700

    First commit

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBUrE5Q_oUVgGF6pPwKab2ulh2c-WauT-s',
+    authDomain: 'tggr-photo-tagger.firebaseapp.com',
+    projectId: 'tggr-photo-tagger',
+    storageBucket: 'tggr-photo-tagger.appspot.com',
+    messagingSenderId: '99293583720',
+    appId: '1:99293583720:web:a79405ac2d1cc3d1861edd',
+};
+
+initializeApp(firebaseConfig);
